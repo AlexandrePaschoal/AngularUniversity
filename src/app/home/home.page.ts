ee0203eabfca9fa4aa10d085e0ae567c2a11ca9d
Alexandre Paschoal <alexandrepasch@gmail.com>
2026-06-27 17:32:37 -0300

    Implementa integração com API de universidades

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UniversityService } from '../services/university';
+import { University } from '../models/university';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,28 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  country: string = '';
+  universities: University[] = [];
+  loading = false;
 
-  constructor() {}
+  constructor(private universityService: UniversityService) {}
 
+  searchUniversities() {
+    if (!this.country.trim()) {
+      return;
+    }
+
+    this.loading = true;
+
+    this.universityService.getUniversities(this.country).subscribe({
+      next: (data) => {
+        this.universities = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.loading = false;
+      },
+    });
+  }
 }
--- src/app/services/university.ts
+++ src/app/services/university.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { University } from '../models/university';
 
 @Injectable({
   providedIn: 'root',
 })
-export class University {
-  
+export class UniversityService {
+  private apiUrl = 'http://universities.hipolabs.com/search?country=';
+
+  constructor(private http: HttpClient) {}
+
+  getUniversities(country: string): Observable<University[]> {
+    return this.http.get<University[]>(`${this.apiUrl}${country}`);
+  }
 }
